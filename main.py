8463806646ea0408f2202b1323dbaa78364b1319
Evan Kopec <ekopec0705@gmail.com>
2022-05-06 11:44:26 -0500

    added main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+print('hello world!')
+import random
+destinations = ['Jupiter', 'Saturn', 'Mercury', 'Neptune']
+restaurants = ['Space Mcdonalds', 'Space Subway', 'Space Taco Bell', 'Space Chick-fil-a']
+transportations = ['land rover', 'hover craft', 'teleportation device']
+entertainments = ['Collect the coolest space rocks', 'Go to the Alien Gladiator Pit ', 'Explore the core of the planet']
+
+
+
+destintion = random.choice(destinations)
+print(destintion)
+restaurant = random.choice(restaurants)
+print(restaurant)
+transport = random.choice(transportations)
+print(transport)
+entertainment = random.choice(entertainments)
+print(entertainment)
+
+
